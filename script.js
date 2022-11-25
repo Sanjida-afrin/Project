ab021073136498f94ea9835609f9d8f5ebfa6bc3
Sorowar Mahabub <sorowarmahabub.bsc.cse.iiuc@gmail.com>
2022-11-26 00:19:28 +0600

    Cart Function Added

--- script.js
+++ script.js
@@ -1,63 +1,75 @@
-var numberError = document.getElementById('number-error');
-var emailError = document.getElementById('email-error');
 
-var boxError = document.getElementById('box-error');
-var totalError = document.getElementById('total-error');
 
-function validMobile() {
-  var phone = document.getElementById('exampleInputmobile').value;
-  if (phone.length == 0) {
-    numberError.innerHTML = 'Please complete this required field.';
-    return false;
-  }
+// Main: Cart Function 
+var count = 0;
+var tprice = 0;
+const cart = (price) => {
+    count = count + 1;
+    document.getElementById('total-products').innerText = count;
+    tprice = tprice + price;
+    document.getElementById('price').innerText = tprice;
+    DeliveryCharge(tprice);
+    // updatePrice(price*102.72);
+    // total();
 
-  if (phone.match(/^[A-Za-z]+$/)) {
-    numberError.innerHTML = 'Must contain only numbers, +()-. and x.';
-    return false;
-  }
-  numberError.innerHTML = ' ';
-  return true;
 }
 
-function validEmail() {
-  var email = document.getElementById('exampleInputEmail1').value;
-  if (email.length == 0) {
-    emailError.innerHTML = 'Please complete this required field.';
-    return false;
-  }
-  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-    emailError.innerHTML = 'Email must be formatted correctly.';
-    return false;
-  }
-  emailError.innerHTML = ' ';
-  return true;
+// Function for Delivery Charge
+var net_price = 0;
+const DeliveryCharge = (newPrice) => {
+    let DeliveryCharge;
+    if (newPrice <= 500) {
+        // return document.getElementById('delivery-charge').innerText = 0;
+        DeliveryCharge = 0;
+    }
+    if (newPrice > 500 && newPrice <= 800) {
+        // document.getElementById('delivery-charge').innerText = 100;
+        DeliveryCharge = 100;
+    }
+    else if (newPrice > 800 && newPrice <= 1000) {
+        // document.getElementById('delivery-charge').innerText = 150;
+        DeliveryCharge = 150;
+    }
+    else if (newPrice >= 1000) {
+        // document.getElementById('delivery-charge').innerText = 200;
+        DeliveryCharge = 200;
+    }
+    document.getElementById('delivery-charge').innerText = DeliveryCharge;
+    net_price = newPrice + DeliveryCharge;
+    document.getElementById('total').innerText = newPrice + DeliveryCharge;
+    tax(net_price);
 }
 
-function validBox() {
-  var box = document.getElementById('text-some').value;
-  if (box.length == 0) {
-    boxError.innerHTML = 'Please complete this required field.';
-    return false;
-  }
-  if (box.match(/^[A-Za-z]+$/)) {
-    boxError.innerHTML = '';
-    return true;
-  }
-  return true;
-}
-function validTotal() {
-  var total = document.getElementById('totalguest').value;
-  if (total.length == 0) {
-    totalError.innerHTML = 'Please complete this required field.';
-    return false;
-  }
 
- 
-  if(total.match(/^[A-Za-z]+$/)){
-    totalError.innerHTML='Please enter a valid number.';
-    return false;
-   }
-   totalError.innerHTML='';
-  return true;
-  
+const tax = (total) => {
+    const taxadd = (total * 0.15);
+    document.getElementById('tax').innerText = taxadd.toFixed(2);
+    const result = taxadd + total;
+    document.getElementById('intotal').innerText = result.toFixed(2);
+}
+
+const orderProducts = () => {
+    const final = document.getElementById('intotal').innerText;
+    alert("Dear Sir," + "\n" + "You have to pay: " + final + "\n" + "Thank You!" + "\n" + "Regrads-" + "\n" + "Twinkles's Shopping Zone");
+    document.getElementById('intotal').innerText = 0;
+    document.getElementById('total-products').innerText = 0;
+    document.getElementById('price').innerText = 0;
+    document.getElementById('delivery-charge').innerText = 0;
+    document.getElementById('total').innerText = 0;
+    document.getElementById('tax').innerText = 0;
+
+    count = 0;
+    tprice = 0;
+    net_price = 0;
+}
+
+
+const chkout = () => {
+    let initial_state = document.getElementById('mycart').style.display;
+    if (initial_state == "block") {
+        document.getElementById('mycart').style.display = "none";
+    }
+    else {
+        document.getElementById('mycart').style.display = "block";
+    }
 }

3fed9cd5ffeb693eceae13f95813078666089bbb
Sanjida Afrin <Sanjidaafrin2707@gmail.com>
2022-11-25 21:57:58 +0600

    done

--- login.php
+++ login.php
@@ -16,19 +16,19 @@ require "db.php";
 <html lang="en">
 
 <head>
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <!-- <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"> -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Twinkles Shoping Zone</title>
-    <link rel="stylesheet" href="style.css">
+    <title>sanjidas Catering</title>
+    <link rel="stylesheet" href="style1.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
-        crossorigin="anonymous"></script>
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
     <!-- font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" />
 </head>
@@ -37,37 +37,29 @@ require "db.php";
 
     <header>
         <!-- <div class="container"> -->
-        <nav class="navbar navbar-expand-lg" id="our-nav">
-            <div class="container-fluid">
-                <a class="navbar-brand ms-5" href="#"><img src="img/logo-nav-final.gif" alt=""> </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+        <nav class="navbar navbar-expand-lg bg-light fixed-top">
+            <div class="container">
+                <button class="navbar-toggler me-5 pe-5" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse me-5" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                           <a class="nav-link" href="index.html">Home</a>
-                        </li>
-                        <!-- <li class="nav-item">
-                            <a class="nav-link" href="index.html">Category</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.html">Review</a>
-                        </li> -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="register.php">Register</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="login.php">Log In</a>
-                        </li>
-                        <button type="button" class=" btn2">Contact Us</button>
+                <div class="collapse navbar-collapse nav-bar justify-content-between" id="navbarTogglerDemo01">
+                    <img src="images/logo.png" class="logo" alt="">
+                    <ul class="navbar-nav mb-lg-0 mt-1">
+                        <a class="navbar-brand px-3" href="index.html">HOME</a>
+                        <a class="navbar-brand px-3" href="Services.html">SERVICES</a>
+                        <a class="navbar-brand px-3" href="Menus.html">MENUS</a>
+                        <a class="navbar-brand px-3" href="Price_list.html">PRICE LIST</a>
+                        <a class="navbar-brand px-2" href="FAQ.html">FAQ</a>
+                        <a class="navbar-brand px-3" href="shop.html">SHOP</a>
+                        <a href="localhost:3360"><button class="navbar-brand btn bg-success text-light px-3"
+                                type="submit">LOGIN</button></a>
                     </ul>
+
                 </div>
             </div>
         </nav>
-        <!-- </div> -->
 
     </header>
 
@@ -79,22 +71,22 @@ require "db.php";
 
 
                 <div class="loginbox">
-                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-                    <img src="img/logo-nav-final.gif" class="pic">
-                    <h1>Customer Login</h1>
+                    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+                        <img src="img/logo-nav-final.gif" class="pic">
+                        <h1>Customer Login</h1>
 
-                    <p>Username</p>
-                    <input type="text" name="uname" placeholder="Enter Username">
-                    <p>Password</p>
-                    <input type="password" name="pass" placeholder="Enter Password">
-                    <p id="psw"><a href="#">Forgot password?</a></p>
-                    <!-- <br> -->
-                    <!-- <input class="btn" type="submit" name="submit" value="Log In"> -->
-                    <!-- <button class="btn btn3" type="submit" name="submit" >Submit</button> -->
-                    <button name="submit" id="one">Submit</button>
-                    <br><br>
-                    <button onclick="signup()" id="two"><a href="register.php">Create Account</a></button>
-               </form>
+                        <p>Username</p>
+                        <input type="text" name="uname" placeholder="Enter Username">
+                        <p>Password</p>
+                        <input type="password" name="pass" placeholder="Enter Password">
+                        <p id="psw"><a href="#">Forgot password?</a></p>
+                        <!-- <br> -->
+                        <!-- <input class="btn" type="submit" name="submit" value="Log In"> -->
+                        <!-- <button class="btn btn3" type="submit" name="submit" >Submit</button> -->
+                        <button name="submit" id="one">Submit</button>
+                        <br><br>
+                        <button onclick="signup()" id="two"><a href="register.php">Create Account</a></button>
+                    </form>
                 </div>
 
 
@@ -135,7 +127,7 @@ require "db.php";
 	    }
 	}
 	?>
-<?php } ?>
+        <?php } ?>
 
     </main>
     <footer>

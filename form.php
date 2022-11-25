0eea6fb02e43b854961ed5983292c32a8c1d3fe7
Sanjida Afrin <Sanjidaafrin2707@gmail.com>
2022-11-25 11:09:09 +0600

    done

--- form.php
+++ form.php
@@ -1,3 +1,7 @@
+<?php ob_start();
+require "db.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -88,37 +92,38 @@
           We will get back to you ASAP</p>
       </div>
       <div>
-        <form class="m-5">
+      <form id="form" action="" method="post" class="m-5">
+        <!-- <form class="m-5"> -->
           <div class="mb-3">
             <label for="exampleInputFullname" class="form-label">Full Name</label>
-            <input type="text" class="form-control" id="exampleInputFullname" aria-describedby="FullnameHelp">
+            <input name="name" type="text" class="form-control" id="exampleInputFullname" aria-describedby="FullnameHelp">
 
           </div>
           <div class="mb-3">
             <label for="exampleInputmobile" class="form-label compulsary">Mobile phone number</label>
-            <input type="text" class="form-control" id="exampleInputmobile" onkeyup="validMobile()">
+            <input name="number" type="text" class="form-control" id="exampleInputmobile" onkeyup="validMobile()">
             <span class="error" id="number-error"></span>
           </div>
           <div class="mb-3 mt-5">
             <label for="exampleInputEmail1" class="form-label compulsary">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onkeyup="validEmail()">
+            <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onkeyup="validEmail()">
             <span class="error" id="email-error"></span>
           </div>
-          <label for="exampleInputEvent" class="form-label mt-5">Event Type</label>
+          <!-- <label for="exampleInputEvent" class="form-label mt-5">Event Type</label>
           <div>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+              <input name="flexRadioDefault" class="form-check-input" type="radio" id="flexRadioDefault1">
               <label class="form-check-label" for="flexRadioDefault1">
                 Private/Social
               </label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+              <input name="flexRadioDefault" class="form-check-input" type="radio"  id="flexRadioDefault2" checked>
               <label class="form-check-label" for="flexRadioDefault2">
                 Corporate
               </label>
             </div>
-          </div>
+          </div> -->
           <label for="exampleInputDate" class="form-label mt-3">Event Date</label>
           <div style="color:#7c98b6 ;">
 
@@ -127,7 +132,7 @@
             <input type="text" id="date" name="date" class="date form-label" placeholder="MM"> <span>/</span>
             <input type="text" id="date" name="date" class="date form-label" placeholder="DD">
           </div>
-          <label for="exampleInputDate" class="form-label mt-3 compulsary">Select Your Requirements</label>
+          <!-- <label for="exampleInputDate" class="form-label mt-3 compulsary">Select Your Requirements</label>
           <div >
             <div class="form-check">
 
@@ -151,27 +156,85 @@
 
             </div>
           
-          </div>
+          </div> -->
           <div class="mt-4">
             <label for="requirement" class="mt-3 compulsary">Write down your menu and other requirements</label>
             <p class="small-letter">Be specific about quantity of your menu items and others (cutlery, crockery, buffet
               dish, and waiter, etc)</p>
-            <textarea name="text-some" id="text-some" class="w-100 form-control" cols="30" rows="10"onkeyup="validBox()"></textarea>
+            <textarea name="text" id="text-some" class="w-100 form-control" cols="30" rows="10"onkeyup="validBox()"></textarea>
            
           </div> <span class="error" id="box-error"></span>
           <div class="mt-4">
             <label for="total" class="compulsary mt-3">Total Number of Guest</label>
             <p class="small-letter">Order Quantity</p>
-            <input type="text" class="form-control" onkeyup="validTotal()" id="totalguest">
+            <input name="gnum" type="text" class="form-control" onkeyup="validTotal()" id="totalguest">
             <span class="error" id="total-error"></span>
           </div>
-          <button type="submit" class="btn btn-danger my-5 px-4">Submit</button>
+          <button name="submit" type="submit" class="btn btn-danger my-5 px-4">Submit</button>
         </form>
       </div>
     </div>
   </section>
-<script src="script.js"></script>
 
+  <?php
+	if(isset($_POST['submit']))
+    {
+        try{
+			$name=$_POST['name'];
+      $number=$_POST['number'];
+      $email=$_POST['email'];
+      $date=$_POST['date'];
+      $text=$_POST['text'];
+      $gnum=$_POST['gnum'];
+	
+		if(empty($name))
+		{
+			echo '<p style="text-align:center;color:red">Please Provide All Information!!!</p>';
+		}
+    else if(empty($number))
+  {
+     echo '<p style="text-align:center;color:red">Please Provide All Information!!!</p>';
+    }
+		else if(empty($email))
+		{
+			echo '<p style="text-align:center;color:red">Please Provide All Information!!!</p>';
+		}
+    else if(empty($date))
+  {
+     echo '<p style="text-align:center;color:red">Please Provide All Information!!!</p>';
+    }
+    else if(empty($text))
+  {
+     echo '<p style="text-align:center;color:red">Please Provide All Information!!!</p>';
+    }
+    else if(empty($gnum))
+  {
+     echo '<p style="text-align:center;color:red">Please Provide All Information!!!</p>';
+    }
+		else
+		{
+      $insert_query="INSERT INTO quotation(Name, Number, Email, Date, Text, Guest) VALUES ('$name','$number','$email','$date','$text','$gnum')";
+			// $insert_query="INSERT INTO user(Name, User_Name, Email, Number, Password) VALUES('$name','$uname','$email','$number','$pass')";
+			$mydb= new database();
+			$response=$mydb->insertdata($insert_query);
+		    if($response==true)
+		    {
+			    header("Location:form.php");
+	        }
+		    else
+		    {
+		        echo '<p style="text-align:center;color:red">Unable To Submit!!!</p>';
+		    }
+	    }
+	}
+	catch(Exception $e)
+		    {
+		    	 echo '<p style="text-align:center;color:red;font-weight:bold">Email Already In Use!!!</p>';
+		    }
+	}
+	?>
+
+<script src="script.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
     crossorigin="anonymous"></script>

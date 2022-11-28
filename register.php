07124761d503463d43233b4dd1e5422556b36ea9
Sanjida Afrin <Sanjidaafrin2707@gmail.com>
2022-11-28 15:22:32 +0600

    first commit

--- register.php
+++ register.php
@@ -29,6 +29,211 @@ require "db.php";
     </script>
     <!-- font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" />
+
+    <style>
+    .signup-left,
+    .signup-right {
+        border-radius: 25px;
+        background: #E5A060;
+        color: white;
+    }
+
+    .register h2 {
+        color: white;
+        font-size: 58px;
+    }
+
+    .form-item input {
+        width: 80%;
+        padding: 12px 20px;
+        margin: 8px 15px;
+        box-sizing: border-box;
+        border: none;
+        background-color: black;
+        color: white;
+        /* background: transparent; */
+        /* background: rgb(135, 123, 123); */
+        /* color: #df0e0e; */
+        /* border-bottom: 0px solid white; */
+    }
+
+    #agree-term {
+        margin-left: -160px;
+    }
+
+    .agree-div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .agree-text {
+        margin-left: -200px;
+    }
+
+    .form-item i {
+        color: white;
+        font-size: 25px;
+        margin-left: 12px;
+    }
+
+    .btn3 {
+        margin-left: 200px;
+        color: white;
+    }
+
+    .signup-right img {
+        border-radius: 15px;
+        padding-bottom: 10px;
+        padding-right: 10px;
+    }
+
+    nav {
+        width: 100%;
+        height: 80px;
+        font-family: alegreya, serif;
+        font-size: medium;
+    }
+
+    .logo {
+        height: 290px;
+        margin-right: 100px;
+        margin-bottom: 10px;
+
+    }
+
+    /* background image nav */
+    .home-img {
+        background-image: url(images/banner/home.jpg);
+    }
+
+    .service-img {
+        background-image: url(images/banner/banner3.png);
+    }
+
+    .menu-img {
+        background-image: url(images/pic.png);
+    }
+
+    .price_list-img {
+        background-image: url(images/corporate.png);
+    }
+
+    .shop-img {
+        background-image: url(images/banner/shop.png);
+    }
+
+    .slide-1 {
+        /* background-image: url(images/banner/desh-catering-itemized-price-list-dhaka.jpg); */
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        transition: background .3s, border .3s, border-radius .3s, box-shadow .3s;
+        transition-duration: 0.3s, 0.3s, 0.3s, 0.3s;
+        transition-timing-function: ease, ease, ease, ease;
+        transition-delay: 0s, 0s, 0s, 0s;
+        transition-property: background, border, border-radius, box-shadow;
+        position: relative;
+        background-attachment: fixed;
+
+    }
+
+    .bannerBlackOverlay {
+        min-height: 100vh;
+        width: 100%;
+        background: rgba(0.2, 0, 0, 0.3);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .bannerTitle {
+        font-weight: 1000;
+        font-size: 100px;
+        color: #fff;
+        text-align: center !important;
+        position: relative;
+    }
+
+    .bannerBtn {
+        background-color: #08af57;
+        padding: 20px 40px;
+        margin-bottom: 20px;
+        font-size: 20px;
+        font-weight: 600;
+    }
+
+    .cate-name {
+        color: #f4cd78;
+        font-family: alegreya sans, Sans-serif;
+        font-size: 58px;
+        font-weight: 600;
+        text-transform: uppercase;
+        line-height: 1.3em;
+        letter-spacing: .5px;
+        margin-top: 200px;
+    }
+
+    /* index */
+
+    .pri-cate {
+        position: relative;
+        height: 100%;
+        width: 100%;
+        background: rgba(79, 79, 79, 0.5);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .pri-cate>div {
+        font-weight: 1000;
+        font-size: 10px;
+        color: #fff;
+        text-align: center !important;
+        position: relative;
+    }
+
+
+    .cate-name2 {
+
+        color: #fff;
+        font-family: alegreya sans sc, Sans-serif;
+        font-size: 35px;
+        font-weight: 600;
+        text-transform: uppercase;
+        font-style: oblique;
+        line-height: 1.2em;
+        letter-spacing: 2.9px;
+        margin-top: 50px;
+
+    }
+
+    .card-img-overlay>h3 {
+        color: #ffce42;
+        font-family: open sans, Sans-serif;
+        font-size: 50px;
+        font-weight: 550;
+    }
+
+    .card-img-overlay>p {
+        text-align: center;
+        color: #fff;
+        font-size: 25px;
+        font-weight: 300;
+        line-height: 1.8em;
+        margin-top: 40px;
+        margin-bottom: 40px;
+    }
+
+    .home-container {
+        margin-top: 40px;
+        text-align: center;
+
+    }
+    </style>
 </head>
 
 <body class="gradient-background">
